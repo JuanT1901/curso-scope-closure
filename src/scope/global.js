b41129770c937172d3da8771eafd10e99bf1b281
Juan Torres <junete190@gmail.com>
2023-05-29 22:44:11 -0500

    First commit

---
+++ src/scope/global.js
@@ -0,0 +1,17 @@
+//variables
+
+var a; //declarando
+var b = 'b'; //declaramos / asignamos
+b = 'bb'; //reasignación
+var a = 'aa' // redeclaración
+
+//Global Scope
+
+var fruit = 'Apple'; //global
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+
---
+++ src/scope/re.js
@@ -0,0 +1,31 @@
+//var
+var firstName;
+firstName = 'Juan';
+console.log(firstName);
+
+var lastName = 'Diego';
+lastName = 'Ana';
+console.log(lastName);
+
+var secondName = 'David';
+var secondName = 'Ana';
+console.log(secondName)
+
+//let
+let fruit = 'Apple';
+fruit = 'Kiwi';
+
+  //let fruit = 'Banana' //No se puede re declarar una variable cuando usamos let, cosa que si se puede hacer con var
+
+//const
+const animal = 'dog'; //declara y asigna
+  //animal = 'cat'; //En el caso de const no podemos reasignar valores a una variable
+
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🚗");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
